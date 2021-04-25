661366840b87948ba8b857d9244ffc9daca07158
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-04-25 15:48:33 +0900

    create cart button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 
 import menuImage from '../../assets/menu.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 function Header() {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMenu</h1>
-        <button type="button">Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={menuImage} alt={'romantic table with food'} />
