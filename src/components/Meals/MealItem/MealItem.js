97f438dec2f2c97eb4db674526e2af4696ff0f4c
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-04-25 17:24:57 +0900

    added individual meal item

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './MealItem.module.css';
+
+const MealItem = ({ name, description, price }) => {
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{`¥${price}`}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
