98d28552c2f844b4004b40a85cdaa670e38ff313
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-04-26 12:49:09 +0900

    change summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -5,11 +5,11 @@ import classes from './Summary.module.css';
 const Summary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Outstanding Food, Delivered To You</h2>
+      <h2>Outstanding food delivered to your door</h2>
       <p>After six successful years in New York, ReactMenu arrive to Tokyo.</p>
       <p>
-        Choose your meal from our selection of available meals and enjoy a
-        delicious lunch or dinner at home.
+        Choose your meal from our favorite selection of the week and enjoy a
+        delicious home-cooked meals at home.
       </p>
     </section>
   );
