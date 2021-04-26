c57480e4e7e36f5f7d8a3cb39de3e23d58f3a80f
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-04-26 16:01:45 +0900

    add dynamic behavior cart size

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({ onClick }) => {
+  const cartContext = useContext(CartContext);
+
+  const cartSize = cartContext.items.reduce(
+    (size, item) => size + item.amount,
+    0
+  );
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{cartSize}</span>
     </button>
   );
 };
